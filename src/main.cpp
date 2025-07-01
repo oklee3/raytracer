4d583e9109720cf91f3ff076c97291684d07d94f
Oliver Lee <oliverlee333@gmail.com>
2025-07-01 12:18:46 -0700

    try different worlds

--- src/main.cpp
+++ src/main.cpp
@@ -10,59 +10,62 @@
 int main() {
     surface_list world;
 
-    auto ground_material = make_shared<lambertian>(color(0.5, 0.5, 0.5));
-    world.add(make_shared<sphere>(point3(0,-1000,0), 1000, ground_material));
+    // Ashen stone ground (diffuse gray)
+    auto ground_material = make_shared<lambertian>(color(0.25, 0.25, 0.25));
+    world.add(make_shared<sphere>(point3(0, -1000, 0), 1000, ground_material));
 
-    for (int a = -11; a < 11; a++) {
-        for (int b = -11; b < 11; b++) {
-            auto choose_mat = random_double();
-            point3 center(a + 0.9*random_double(), 0.2, b + 0.9*random_double());
-
-            if ((center - point3(4, 0.2, 0)).length() > 0.9) {
-                shared_ptr<material> sphere_material;
-
-                if (choose_mat < 0.8) {
-                    // diffuse
-                    auto albedo = color::random() * color::random();
-                    sphere_material = make_shared<lambertian>(albedo);
-                    world.add(make_shared<sphere>(center, 0.2, sphere_material));
-                } else if (choose_mat < 0.95) {
-                    // metal
-                    auto albedo = color::random(0.5, 1);
-                    auto fuzz = random_double(0, 0.5);
-                    sphere_material = make_shared<metal>(albedo, fuzz);
-                    world.add(make_shared<sphere>(center, 0.2, sphere_material));
-                } else {
-                    // glass
-                    sphere_material = make_shared<dielectric>(1.5);
-                    world.add(make_shared<sphere>(center, 0.2, sphere_material));
-                }
-            }
+    // Ruined spires rising from the ground (tall vertical sphere stacks)
+    for (int i = -5; i <= 5; i += 2) {
+        point3 base(i, 0.4, -4 + random_double(-1, 1));
+        for (int j = 0; j < 3; ++j) {
+            auto stone = make_shared<lambertian>(color(0.2 + 0.05*random_double(), 0.2, 0.2));
+            point3 center = base + vec3(0, j * 0.6, 0);
+            world.add(make_shared<sphere>(center, 0.3, stone));
         }
     }
 
-    auto material1 = make_shared<dielectric>(1.5);
-    world.add(make_shared<sphere>(point3(0, 1, 0), 1.0, material1));
+    // Broken remnants scattered across the field
+    for (int i = 0; i < 30; ++i) {
+        point3 center(random_double(-10, 10), 0.2, random_double(-10, 10));
+        if ((center - point3(0,0,0)).length() < 1.5) continue;
 
-    auto material2 = make_shared<lambertian>(color(0.4, 0.2, 0.1));
-    world.add(make_shared<sphere>(point3(-4, 1, 0), 1.0, material2));
+        auto rubble_material = make_shared<lambertian>(color(0.2 + 0.1*random_double(), 0.2, 0.2));
+        world.add(make_shared<sphere>(center, 0.2, rubble_material));
+    }
 
-    auto material3 = make_shared<metal>(color(0.7, 0.6, 0.5), 0.0);
-    world.add(make_shared<sphere>(point3(4, 1, 0), 1.0, material3));
+    // Rusted armor debris (random metal spheres)
+    for (int i = 0; i < 10; ++i) {
+        point3 center(random_double(-8, 8), 0.2, random_double(-8, 8));
+        auto metal_color = color(0.4, 0.2 + 0.2*random_double(), 0.1); // dark bronze/red
+        auto rusted = make_shared<metal>(metal_color, 0.5);
+        world.add(make_shared<sphere>(center, 0.3, rusted));
+    }
+
+    // Central relic: floating glass orb
+    auto relic_material = make_shared<dielectric>(1.5);
+    world.add(make_shared<sphere>(point3(0, 1, 0), 1.0, relic_material));
+
+    // Hero’s grave marker: solid matte statue
+    auto grave_material = make_shared<lambertian>(color(0.15, 0.15, 0.2));
+    world.add(make_shared<sphere>(point3(-4, 1, 0), 1.0, grave_material));
+
+    // Forgotten knight’s helm: polished metal
+    auto knight_helm = make_shared<metal>(color(0.8, 0.7, 0.6), 0.1);
+    world.add(make_shared<sphere>(point3(4, 1, 0), 1.0, knight_helm));
 
     viewport view;
 
     view.aspect_ratio      = 16.0 / 9.0;
-    view.image_width       = 400;
-    view.samples_per_pixel = 10;
+    view.image_width       = 1200;
+    view.samples_per_pixel = 50;
     view.max_depth         = 50;
 
-    view.vfov     = 20;
-    view.lookfrom = point3(13,2,3);
-    view.lookat   = point3(0,0,0);
-    view.vup      = vec3(0,1,0);
+    view.vfov     = 25;
+    view.lookfrom = point3(13, 3, 6);
+    view.lookat   = point3(0, 1, 0);
+    view.vup      = vec3(0, 1, 0);
 
-    view.defocus_angle = 0.6;
+    view.defocus_angle = 0.2;
     view.focus_dist    = 10.0;
 
     view.render(world);
